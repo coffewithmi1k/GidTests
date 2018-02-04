aac6908919ef6dcdfc5f88f2e0991fa00f0ffcff
coffe <coffeacc10@gmail.com>
2018-02-04 14:03:35 +0200

    Initial commit

---
+++ src/test/java/SampleTest2.java
@@ -0,0 +1,39 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.*;
+
+public class SampleTest2 {
+
+    private WebDriver driver;
+
+    @BeforeMethod
+    public void setUp(){
+        // driver = new FirefoxDriver();
+        driver = new ChromeDriver();
+    }
+
+    @Test
+    public  void first(){
+        driver.get("Https:google.com");
+        System.out.println("First");
+    }
+    @Test
+    public  void second()   {
+        driver.get("Https:facebook.com");
+        System.out.println("second");
+    }
+
+    @Test
+    public  void third(){
+        driver.get("Https:twitter.com");
+        System.out.println("third");
+    }
+
+
+    @AfterMethod
+    public void close(){
+        driver.close();
+    }
+
+}
